3e005d5f322eed5a864af0fb9ab8bb475e8b229d
Mike Zaleski <me@mikezaleski.com>
2022-01-12 22:59:42 -0500

    add progress bag with conditional coloring

--- src/components/BudgetCard.js
+++ src/components/BudgetCard.js
@@ -1,4 +1,4 @@
-import { Card } from "react-bootstrap";
+import { Card, ProgressBar } from "react-bootstrap";
 import { currencyFormatter } from "../utils";
 
 export default function BudgetCard({ name, amount, max }) {
@@ -14,7 +14,21 @@ export default function BudgetCard({ name, amount, max }) {
             </span>
           </div>
         </Card.Title>
+        <ProgressBar
+          className="rounded-pill"
+          variant={getProgressBarVariant(amount, max)}
+          min={0}
+          max={max}
+          now={amount}
+        />
       </Card.Body>
     </Card>
   );
+}
+
+function getProgressBarVariant(amount, max) {
+  const ratio = amount / max;
+  if (ratio < 0.5) return "primary";
+  if (ratio < 0.75) return "warning";
+  return "danger";
 }
