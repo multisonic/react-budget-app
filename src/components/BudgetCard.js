6be2453b321a2be6c07c032ccfe4c4b62ad79e5c
Mike Zaleski <me@mikezaleski.com>
2022-01-12 22:55:15 -0500

    add basic budget card styles

--- src/components/BudgetCard.js
+++ src/components/BudgetCard.js
@@ -5,10 +5,13 @@ export default function BudgetCard({ name, amount, max }) {
   return (
     <Card>
       <Card.Body>
-        <Card.Title>
-          <div>{name}</div>
-          <div>
-            {currencyFormatter.format(amount)} / {currencyFormatter.format(max)}
+        <Card.Title className="d-flex justify-content-between align-items-baseline fw-normal mb-3">
+          <div className="me-2">{name}</div>
+          <div className="d-flex align-items-baseline">
+            {currencyFormatter.format(amount)}
+            <span className="text-muted fs-6 ms-1">
+              / {currencyFormatter.format(max)}
+            </span>
           </div>
         </Card.Title>
       </Card.Body>
