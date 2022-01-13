57380e4e833f93e7e80ef5fb78f024f6d43a8122
Mike Zaleski <me@mikezaleski.com>
2022-01-12 22:45:50 -0500

    Basic Component HTML and CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Button, Container, Stack } from "react-bootstrap";
+
 function App() {
-  return "hello";
+  return (
+    <Container className="my-4">
+      <Stack direction="horizontal" gap="2" className="mb-4">
+        <h1 className="me-auto">Budgets</h1>
+        {/* me-auto keeps h1 fixed to the left by adding space onto the end of the element */}
+        <Button variant="primary">Add Budget</Button>
+        <Button variant="outline-primary">Add Expense</Button>
+      </Stack>
+
+      {/* custom css grid below */}
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+          gap: "1rem",
+          alignItems: "flex-start",
+        }}
+      ></div>
+    </Container>
+  );
 }
 
 export default App;
